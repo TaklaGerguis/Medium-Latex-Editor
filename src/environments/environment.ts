907a288dfad9bb8b7f8ec3c03b15a7ec8537d8b5
Takla Gerguis <takla.gerguis@brightskiesinc.com>
2020-11-28 17:51:42 +0200

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAetnAZgqKhgWpAhVzPIes8khYOVSw4UjU',
+    authDomain: 'pencil-4e806.firebaseapp.com',
+    databaseURL: 'https://pencil-4e806.firebaseio.com',
+    projectId: 'pencil-4e806',
+    storageBucket: 'pencil-4e806.appspot.com',
+    messagingSenderId: '727096775143',
+    appId: '1:727096775143:web:ef34b8364eb7e0f565ad42',
+    measurementId: 'G-BHKMPDPW6Y'
+  }
 };
 
 /*
